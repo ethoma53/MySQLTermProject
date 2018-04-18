c370b9896633598e5cffd37b000951ef6d9d7f9f
ethoma53 <32469925+ethoma53@users.noreply.github.com>
2018-04-18 14:44:04 -0400

    uploaded .sql report

--- sql scripts/numRejected2017.sql
+++ sql scripts/numRejected2017.sql
@@ -3,6 +3,6 @@ use cartProject;
 SELECT COUNT(*)
 FROM
 	(SELECT DISTINCT userId
-FROM archive
-WHERE status LIKE '%rejected%'
-AND decisionDate > 2017-01-01) AS archive;
+	FROM archive
+	WHERE status LIKE '%rejected%'
+	AND decisionDate > 2017-01-01) AS archive;
