b51e98c0490e37330a8ddaebfba2257591d368eb
ethoma53 <32469925+ethoma53@users.noreply.github.com>
2018-04-19 10:41:13 -0400

    Add files via upload

--- sql scripts/applicationsInProgress.sql
+++ sql scripts/applicationsInProgress.sql
@@ -1,8 +1,12 @@
-SELECT a.appId, b.lastName, b.FirstName, c.jobName, a.status, a.dateApplied
+SELECT a.appId, b.lastName, b.firstName, c.jobName, a.status, a.dateApplied, datediff(NOW(), a.dateApplied) as NumDaysInQueue
 FROM cartproject.application as a
 INNER JOIN cartproject.user as b
 ON a.User_userId = b.userId
 INNER JOIN cartproject.job as c
 ON a.Job_jobId = c.jobId
-WHERE a.status IN ('not opened ', ' pending ')
+WHERE (a.status LIKE ('%not opened%') OR a.status LIKE ('%pending%'))
+AND datediff(NOW(), a.dateApplied) >= 30
+AND a.User_userId NOT IN (SELECT User_userId 
+						  FROM employee)
 ORDER BY dateApplied;
+
